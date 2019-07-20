620de5167f2599a7f102e6eff5debc5647c49424
Memo1973 <53022758+Memo1973@users.noreply.github.com>
2019-07-19 18:31:53 -0600

    Add files via upload

--- src/joaquin/Main.java
+++ src/joaquin/Main.java
@@ -1,33 +1,36 @@
-package joaquin;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        //Queen.java
-        System.out.println("You are being shouted at by the Mad Hatter.");
-        // play audio at 1:10:36 - 1:10:44 via AudioParsing.java
-        System.out.println("'No room, no room, no room, no room, no room!\' is what you hear");
+package Tello;
 
 
 
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 
+public class Main{
 
+    public static void main(String avg[]) throws IOException
+    {
+        Main abc=new Main();
+    }
 
-
-
-
-
-
-
-
-
-
-
+    public Main() throws IOException
+    {
+        BufferedImage img=ImageIO.read(new File("C:\\Users\\tello\\Documents\\csis 1400\\img\\alice_img1_3_30.png"));
+        ImageIcon icon=new ImageIcon(img);
+        JFrame frame=new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(650,450);
+        JLabel lbl=new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
